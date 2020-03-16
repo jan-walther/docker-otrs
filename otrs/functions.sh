1171f61c7fc6d990d5d65823d91db81c29b2dfcf
jan <62257247+jan-walther@users.noreply.github.com>
2020-03-16 18:47:04 +0100

    Switch to pgsql

--- otrs/functions.sh
+++ otrs/functions.sh
@@ -121,7 +121,7 @@ function restore_backup() {
   add_config_value "DatabaseHost" ${OTRS_DB_HOST}
   add_config_value "DatabasePort" ${OTRS_DB_PORT}
   add_config_value "Database" ${OTRS_DB_NAME}
-  add_config_value "DatabaseDSN" "DBI:Pg:dbname=$Self->{Database};host=$Self->{DatabaseHost};"
+  add_config_value "DatabaseDSN" "DBI:Pg:dbname=${OTRS_DB_NAME};host=${OTRS_DB_HOST};"
 
   #Check first that the backup file exists
   restore_file="${OTRS_BACKUP_DIR}/${OTRS_BACKUP_DATE}"
@@ -229,7 +229,7 @@ function setup_otrs_config() {
   add_config_value "DatabaseHost" ${OTRS_DB_HOST}
   add_config_value "DatabasePort" ${OTRS_DB_PORT}
   add_config_value "Database" ${OTRS_DB_NAME}
-  add_config_value "DatabaseDSN" "DBI:Pg:dbname=$Self->{Database};host=$Self->{DatabaseHost};"
+  add_config_value "DatabaseDSN" "DBI:Pg:dbname=${OTRS_DB_NAME};host=${OTRS_DB_HOST};"
   #Set general configuration values
   [ ! -z "${OTRS_LANGUAGE}" ] && add_config_value "DefaultLanguage" ${OTRS_LANGUAGE}
   [ ! -z "${OTRS_TIMEZONE}" ] && add_config_value "OTRSTimeZone" ${OTRS_TIMEZONE} && add_config_value "UserDefaultTimeZone" ${OTRS_TIMEZONE}
